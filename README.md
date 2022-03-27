86c0e3d5669a2ce62d72009106a5ef7996f945a0
Gabriel Noeberto <sn_gabriel@outlook.com>
2022-03-27 18:05:20 -0300

    Processo finalizado

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # node-passport
+
+npm instal passpot
+npm install -D @types/passport
+
+npm install passport-http
+npm install -D @types/passport-http
--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import path from 'path';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import apiRoutes from './routes/api';
+import passport from 'passport';
 
 dotenv.config();
 
@@ -15,17 +16,27 @@ server.use(express.urlencoded({ extended: true }));
 
 server.get('/ping', (req: Request, res: Response) => res.json({ pong: true }));
 
+server.use(passport.initialize());
+
 server.use(apiRoutes);
 
 server.use((req: Request, res: Response) => {
-    res.status(404);
-    res.json({ error: 'Endpoint não encontrado.' });
+   res.status(404);
+   res.json({ error: 'Endpoint não encontrado.' });
 });
 
 const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
-    res.status(400); // Bad Request
-    console.log(err);
-    res.json({ error: 'Ocorreu algum erro.' });
+   if (err.status) {
+      res.status(err.status)
+   } else {
+      res.status(400); // Bad Request
+   }  
+   
+   if(err.message){
+      res.json({ error: err.message })
+   } else {
+      res.json({ error: 'Ocorreu algum erro.' });
+   }
 }
 server.use(errorHandler);
 
